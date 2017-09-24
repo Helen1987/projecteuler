aba7aca65879e7a07a9ffb17ec54ee61b02693c6
Helen1987 <kastsiukavets.alena@gmail.com>
2017-09-24 16:01:29 +0300

    feat: task 2 - even Fibonacci sum

---
+++ Euler/Euler/1-10/Task2 - Even Fibonacci numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class Task2
+    {
+        int first = 1;
+        int second = 2;
+        int temp;
+        long sum;
+
+        private int getNextFibonnachi(int prev, int current)
+        {
+            return prev + current;
+        }
+
+        public long Run(long maxValue)
+        {
+            sum = second;
+            while (true)
+            {
+                temp = getNextFibonnachi(first, second);
+                if (temp > maxValue)
+                    return sum;
+
+                if (temp % 2 == 0)
+                {
+                    sum += temp;
+                }
+                first = second;
+                second = temp;
+            }
+        }
+    }
+}
--- Euler/UnitTests/UnitTest1-10.cs
+++ Euler/UnitTests/UnitTest1-10.cs
@@ -15,5 +15,13 @@ namespace UnitTests
             Console.WriteLine();
             Assert.AreEqual(233168, task.Run(max), $"Multiples of 3 and 5 below {max} are incorrect");
         }
+
+        [TestMethod]
+        public void TestFibbonacci()
+        {
+            var task = new Task2();
+            int max = 4000000;
+            Console.WriteLine(4613732, task.Run(max), $"Sum of Even Fibonacci numbers below {max} is incorrect");
+        }
     }
 }
