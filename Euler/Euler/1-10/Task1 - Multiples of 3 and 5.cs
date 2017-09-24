f9a39e1bab52f40cdb3d3a8e6523bce588dd4c37
Helen1987 <kastsiukavets.alena@gmail.com>
2017-09-24 15:29:51 +0300

    feat: task1

---
+++ Euler/Euler/1-10/Task1 - Multiples of 3 and 5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    public class Task1
+    {
+        private bool IsMultiple(int number)
+        {
+            return number % 5 == 0 || number % 3 == 0;
+        }
+
+        public int Run(int maxNumber)
+        {
+            int sum = 0;
+            for (int i = 1; i < maxNumber; ++i)
+            {
+                if (IsMultiple(i))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
--- Euler/UnitTests/UnitTest1.cs
+++ Euler/UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Euler;
 
 namespace UnitTests
 {
@@ -7,8 +8,12 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMultiples()
         {
+            var task = new Task1();
+            int max = 1000;
+            Console.WriteLine();
+            Assert.AreEqual(233168, task.Run(max), $"Multiples of 3 and 5 below {max} are incorrect");
         }
     }
 }
