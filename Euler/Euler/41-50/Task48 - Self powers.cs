a43395d7b76cc96e1a6e20e06411dc73711fd24a
Alena Kastsiukavets <kastsiukavets.alena@gmail.com>
2017-10-09 15:50:53 +0300

    feat: Task48 - Self powers

---
+++ Euler/Euler/41-50/Task48 - Self powers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    /// <summary>
+    /// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+    /// Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+    /// </summary>
+    public class Task48
+    {
+        const long MODULO = 10000000000;
+
+        public long Run(int maxNumber)
+        {
+            long sum = 0;
+            for (int i = 1; i <= maxNumber; ++i)
+            {
+                long powerOfNumber = 1;
+                for (int power = 1; power <= i; ++power)
+                {
+                    powerOfNumber *= i;
+                    powerOfNumber %= MODULO;
+                }
+                sum += powerOfNumber;
+                sum %= MODULO;
+            }
+            return sum;
+        }
+    }
+}
